e0bbb252552c7f3e23a77ac923ad798949ada22d
AbhilakshSinghReen <abhilakshreen@gmail.com>
2023-12-07 21:09:00 +0530

    Save metadata for segments: geometric origin and is ROI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import nibabel as nib
 import numpy as np
+import json
 from skimage import measure
 
 
@@ -43,13 +44,14 @@ def write_obj_file(output_path, verts, faces, normals, values):
 def get_average_index_of_1_values(volume_data):
     val_1_indices = np.where(volume_data == 1)
     val_1_average_index = np.mean(val_1_indices, axis=1)
-    val_1_average_index
-    
-def save_segment_as_object(output_path, _volume_data, voxel_spacing, segment_value):
+    return val_1_average_index.tolist()
+
+def save_segment_as_object(output_file_path, _volume_data, voxel_spacing, segment_value):
    volume_data = _volume_data.copy()
    volume_data[volume_data != segment_value] = 0
    volume_data[volume_data == segment_value] = 1
    verts, faces, normals, values = get_polygonal_mesh_from_voxel_data(volume_data, voxel_spacing)
+
    ###
    # TODO (Dr. Amit): (verts, faces, normals, values) represent the mesh generated. We have to:
    # Remove excess polygons/triangles from this Mesh
@@ -58,17 +60,28 @@ def save_segment_as_object(output_path, _volume_data, voxel_spacing, segment_val
 
    # After making changes to the mesh you can write the file using the function below if you have (verts, faces, normals) 
    # or you can write .obj using the library you used to edit the mesh.
-   write_obj_file(output_path, verts, faces, normals, values)
+   ###
 
-def total_segmentator_output_to_objs(ts_out_file_path, objs_save_folder):
+   
+   write_obj_file(output_file_path, verts, faces, normals, values)
+
+   segment_geometric_origin = get_average_index_of_1_values(volume_data)
+   return segment_geometric_origin
+
+def total_segmentator_output_to_objs(ts_out_file_path, output_folder, segments_of_interest):
+    segments_of_interest = set(segments_of_interest)
+
     nifti_file = nib.load(ts_out_file_path)
     voxel_spacing = nifti_file.header.get_zooms()
     volume_data = nifti_file.get_fdata()
 
-    # print("Full volume")
-    # ndarr_metadata(volume_data)
-
     segment_values = np.unique(volume_data)
+    output_metadata = {
+        'meshes': [],
+    }
+    output_metadata_json_path = os.path.join(output_folder, f"metadata.json")
+    with open(output_metadata_json_path, 'w') as file:
+        json.dump(output_metadata, file, indent=2)
 
     for segment_value in segment_values:
         if int(segment_value) == 0: # background
@@ -79,9 +92,17 @@ def total_segmentator_output_to_objs(ts_out_file_path, objs_save_folder):
             print(f"No name found for segment value {segment_value}. Skipping this segment...")
             continue
 
-        # print(f"Segment name: {segment_name}")
-        segment_obj_output_path = os.path.join(objs_save_folder, f"{segment_name}.obj")
-        save_segment_as_object(segment_obj_output_path, volume_data, voxel_spacing, segment_value)
+        segment_obj_output_path = os.path.join(output_folder, f"{segment_name}.obj")
+        segment_geometric_origin = save_segment_as_object(segment_obj_output_path, volume_data, voxel_spacing, segment_value)
+
+        output_metadata['meshes'].append({
+            'name': f"{segment_name}.obj",
+            'geometricOrigin': json.dumps(segment_geometric_origin),
+            'isROI': segment_name in segments_of_interest,
+        })
+        with open(output_metadata_json_path, 'w') as file:
+            json.dump(output_metadata, file, indent=2)
+
         print(f"Saved segment {segment_name} as {segment_name}.obj")
 
 if __name__ == "__main__":
@@ -89,4 +110,4 @@ if __name__ == "__main__":
     output_dir = "outputs/1-tsoc"
     os.makedirs(output_dir, exist_ok=True)
 
-    total_segmentator_output_to_objs(file_path, output_dir)
+    total_segmentator_output_to_objs(file_path, output_dir, [])
